0053192e903dc242e9f1daff36606bdfeb90e114
Mike Krisher <mike@mikekrisher.com>
2025-11-11 09:18:11 -0500

    Adds additional book info and handles no book found response

--- src/book/google.rs
+++ src/book/google.rs
@@ -9,28 +9,67 @@ struct GoogleBooksResponse {
 struct BookItem {
     #[serde(rename = "volumeInfo")]
     volume_info: VolumeInfo,
+    #[serde(rename = "saleInfo")]
+    sale_info: SaleInfo,
 }
 
 #[derive(Deserialize, Debug)]
 struct VolumeInfo {
     title: String,
     authors: Vec<String>,
+    publisher: String,
+    #[serde(rename = "publishedDate")]
+    published_date: String,
     #[serde(rename = "pageCount")]
     page_count: u32,
     categories: Vec<String>,
+    description: String,
+    #[serde(rename = "contentVersion")]
+    content_version: String,
+    #[serde(rename = "imageLinks")]
+    image_links: ImageLinks,
+    language: String,
+    #[serde(rename = "previewLink")]
+    preview_link: String,
+    #[serde(rename = "infoLink")]
+    info_link: String,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Clone)]
+pub struct ImageLinks {
+    #[serde(rename = "smallThumbnail")]
+    small_thumbnail: String,
+    thumbnail: String,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Clone)]
+pub struct SaleInfo {
+    country: String,
+    saleability: String,
+    #[serde(rename = "isEbook")]
+    is_ebook: bool,
 }
 
 #[derive(Deserialize, Debug, PartialEq)]
 pub struct BookInfo {
     pub title: String,
     pub authors: String,
+    pub publisher: String,
+    pub published_date: String,
     pub page_count: u32,
     pub categories: String,
+    pub description: String,
+    pub content_version: String,
+    pub image_links: ImageLinks,
+    pub language: String,
+    pub preview_link: String,
+    pub info_link: String,
+    pub sale_info: SaleInfo,
 }
 
 static URL: &str = "https://www.googleapis.com/books/v1/volumes?q=isbn";
 
-pub async fn book(isbn: &str) -> Result<BookInfo, Box<dyn std::error::Error>> {
+pub async fn book(isbn: &str) -> Result<Option<BookInfo>, Box<dyn std::error::Error>> {
     println!("Fetching book with ISBN: {}", isbn);
 
     // Clean the ISBN by removing any hyphens or spaces
@@ -42,6 +81,7 @@ pub async fn book(isbn: &str) -> Result<BookInfo, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     let response = client.get(&url).send().await?;
 
+    // NOTE: response returns { "kind": "books#volumes", "totalItems": 0 } if book is not found
     let books_response: GoogleBooksResponse = response.json().await?;
 
     if let Some(items) = books_response.items {
@@ -51,9 +91,20 @@ pub async fn book(isbn: &str) -> Result<BookInfo, Box<dyn std::error::Error>> {
                 authors: first_book.volume_info.authors.join(", ").clone(),
                 page_count: first_book.volume_info.page_count,
                 categories: first_book.volume_info.categories.join(", ").clone(),
+                publisher: first_book.volume_info.publisher.clone(),
+                published_date: first_book.volume_info.published_date.clone(),
+                description: first_book.volume_info.description.clone(),
+                content_version: first_book.volume_info.content_version.clone(),
+                image_links: first_book.volume_info.image_links.clone(),
+                language: first_book.volume_info.language.clone(),
+                preview_link: first_book.volume_info.preview_link.clone(),
+                info_link: first_book.volume_info.info_link.clone(),
+                sale_info: first_book.sale_info.clone(),
             };
-            return Ok(result);
+            return Ok(Some(result));
         }
+    } else {
+        return Ok(None);
     }
 
     Err("No book found for this ISBN".into())
--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,13 @@ mod tests {
     async fn it_successfully_gets_book_info_from_google() {
         let result = book::google::book("978-1718503106").await;
         match &result {
-            Ok(book_info) => {
+            Ok(result) => {
                 // println!(
                 //     "Found book: {} by {} {} pages in the {} category",
                 //     book_info.title, book_info.authors, book_info.page_count, book_info.categories
                 // );
 
-                assert_eq!(
-                    book_info.title,
-                    "The Rust Programming Language, 2nd Edition"
-                );
+                assert!(result.is_some());
             }
             Err(e) => println!("Error: {}", e),
         }
@@ -27,16 +24,28 @@ mod tests {
         // Test with ISBN-10 format
         let result_isbn10 = book::google::book("1718503105").await;
         match &result_isbn10 {
-            Ok(book_info) => {
+            Ok(result) => {
                 // println!("Found book: {} by {}", book_info.title, book_info.authors);
 
-                assert_eq!(
-                    book_info.title,
-                    "The Rust Programming Language, 2nd Edition"
-                );
+                assert!(result.is_some());
             }
             Err(e) => println!("Error: {}", e),
         }
         assert!(result_isbn10.is_ok());
+    }
+
+    // Using an unknown ISBN
+    #[tokio::test]
+    async fn it_returns_none() {
+        let result = book::google::book("978").await;
+        match &result {
+            Ok(result) => {
+                println!("Book not found",);
+
+                assert!(result.is_none());
+            }
+            Err(e) => println!("Error: {}", e),
+        }
+        assert!(result.is_ok());
     }
 }
